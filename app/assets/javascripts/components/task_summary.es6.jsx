02d32a6dc475378953586cd4abc8c9c3bb198bd5
blueplanet <erguolinge@gmail.com>
2016-12-23 16:40:02 +0900

    Add sum fun

--- app/assets/javascripts/components/task_summary.es6.jsx
+++ app/assets/javascripts/components/task_summary.es6.jsx
@@ -1,11 +1,18 @@
 class TaskSummary extends React.Component {
-  estimateTotal
+  estimateTotal () {
+    var total = 0;
+    for (var task of this.props.tasks) {
+      total += task.estimate;
+    }
+
+    return total;
+  }
 
   render () {
     return(
       <tr>
         <td className='text-right'>合計</td>
-        <td></td>
+        <td>{this.estimateTotal()}</td>
       </tr>
     );
   }
