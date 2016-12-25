a8510b6b85a4e7381867616a07dca7701bd86ed2
blueplanet <erguolinge@gmail.com>
2016-12-25 09:32:47 +0900

    デザイン調整

--- app/assets/javascripts/components/task.es6.jsx
+++ app/assets/javascripts/components/task.es6.jsx
@@ -1,10 +1,14 @@
 class Task extends React.Component {
   render () {
     return(
-      <tr className='task-item' key={this.props.task.id}>
-        <td>{this.props.task.name}</td>
-        <td>{this.props.task.estimate}</td>
-      </tr>
+      <div className='row task-item' key={this.props.task.id}>
+        <div className='col-md-8'>
+          <div className='form-control-static text-center'>{this.props.task.name}</div>
+        </div>
+        <div className='col-md-2'>
+          <div className='form-control-static text-center'>{this.props.task.estimate}</div>
+        </div>
+      </div>
     );
   }
 }
--- app/assets/javascripts/components/task_list.es6.jsx
+++ app/assets/javascripts/components/task_list.es6.jsx
@@ -7,18 +7,18 @@ class TaskList extends React.Component {
     });
 
     return(
-      <table className='table task-list'>
-        <thead>
-          <tr>
-            <td>タスク名</td>
-            <td>見積（分）</td>
-          </tr>
-        </thead>
-        <tbody>
-          {taskNodes}
-          <TaskSummary tasks={this.props.tasks} />
-        </tbody>
-      </table>
+      <div className='task-list'>
+        <div className='row'>
+          <div className='col-md-8'>
+            <div className='form-control-static text-center'>タスク名</div>
+          </div>
+          <div className='col-md-2'>
+            <div className='form-control-static text-center'>見積（分）</div>
+          </div>
+        </div>
+        {taskNodes}
+        <TaskSummary tasks={this.props.tasks} />
+      </div>
     );
   }
 }
