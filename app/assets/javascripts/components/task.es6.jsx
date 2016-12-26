0f3a961adb5a26d01207ac1d30c9c20300cb10c6
blueplanet <erguolinge@gmail.com>
2016-12-27 06:28:12 +0900

    何故か編集できないようになってる

--- app/assets/javascripts/components/task.es6.jsx
+++ app/assets/javascripts/components/task.es6.jsx
@@ -2,21 +2,33 @@ class Task extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {editMode: false};
+    this.state = {editMode: false, task: props.task};
   }
 
-  onClickEdit () {
+  onClickEdit (e) {
+    e.preventDefault();
     this.setState({editMode: true});
+  }
+
+  onChangeName (e) {
+    // this.setState({name: e.target.value.trim()});
+    console.log('on change name!!!!!!!!!!!!!!!!!!!!!!!!!');
+  }
+
+  onChangeEstimate (e) {
+    this.setState({estimate: parseInt(e.target.value.trim())});
   }
 
   render () {
     return(
       <div className='row task-item' key={this.props.task.id}>
         <div className='col-md-8'>
-          <div className='form-control-static text-center {this.state.editMode ? "hide" : ""}'>{this.props.task.name}</div>
+          <div className={'form-control-static text-center ' + (this.state.editMode ? 'hide' : '')}>{this.props.task.name}</div>
+          <input className={'form-control ' + (this.state.editMode ? '' : 'hide')} type='text' value={this.state.task.name} onChange={this.onChangeName.bind(this)} />
         </div>
         <div className='col-md-2'>
-          <div className='form-control-static text-center'>{this.props.task.estimate}</div>
+          <div className={'form-control-static text-center ' + (this.state.editMode? 'hide' : '')}>{this.props.task.estimate}</div>
+          <input className={'form-control ' + (this.state.editMode ? '' : 'hide')} type='text' value={this.props.task.estimate} onChange={this.onChangeEstimate.bind(this)} />
         </div>
         <div className='col-md-2'>
           <div className='form-control-static text-center'>
